41830f769c4b8fce4e533add86d2cee767ecfe21
Nathanael Anderson <nathan@master-technology.com>
2016-10-22 00:11:50 -0500

    1. Swapped iOS to actually use a 4 up, so it looks like the camera app; also will automatically add an additional columns based on width. 2. Added feature newestFirst to options which will inverse the images

--- nativescript-imagepicker/images.ios.ts
+++ nativescript-imagepicker/images.ios.ts
@@ -8,6 +8,11 @@ export function pageLoaded(args) {
     page = args.object;
     page.bindingContext = page.navigationContext;
     list = page.getViewById("images-list");
+
+    // Get the current Size, and then adjust the number of columns based on it...
+    var size = iOSProperty(UIScreen, UIScreen.mainScreen).bounds.size.width;
+    list.listViewLayout.spanCount = Math.floor(size / 80);
+
 }
 
 export function done(args) {
@@ -16,4 +21,13 @@ export function done(args) {
     topmost.goBack();
 
     page.bindingContext.imagePicker.done();
+}
+
+function iOSProperty(_this, property) {
+    if (typeof property === "function") {
+        return property.call(_this);
+    }
+    else {
+        return property;
+    }
 }
