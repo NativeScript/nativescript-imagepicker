3e52d7a2fbecbec54ffe60be65d3d76f8b057168
Ignacio Larrañaga <ignaciolarranaga@gmail.com>
2016-11-11 07:01:32 -0300

    Removing helpers for webpack compatibility.

--- nativescript-imagepicker/viewmodel.ios.js
+++ nativescript-imagepicker/viewmodel.ios.js
@@ -1,9 +1,4 @@
 "use strict";
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
 var data_observable = require("data/observable");
 var data_observablearray = require("data/observable-array");
 var frame = require("ui/frame");
