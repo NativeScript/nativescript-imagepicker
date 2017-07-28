ee4053acbbdf0fede64b6f83aa2b13255c089215
Veselina Radeva <Veselina.Radeva@progress.com>
2017-07-28 14:32:48 +0300

    temp: comment out tests for now

--- demo-angular/app/tests/tests.js
+++ demo-angular/app/tests/tests.js
@@ -1,12 +1,12 @@
-var YourPlugin = require("nativescript-yourplugin").YourPlugin;
-var yourPlugin = new YourPlugin();
+// var ImagePicker = require("nativescript-imagepicker").ImagePicker;
+// var imagePicker = new ImagePicker();
 
-describe("greet function", function() {
-    it("exists", function() {
-        expect(yourPlugin.greet).toBeDefined();
-    });
+// describe("greet function", function() {
+//     it("exists", function() {
+//         expect(yourPlugin.greet).toBeDefined();
+//     });
 
-    it("returns a string", function() {
-        expect(yourPlugin.greet()).toEqual("Hello, NS");
-    });
-});
+//     it("returns a string", function() {
+//         expect(yourPlugin.greet()).toEqual("Hello, NS");
+//     });
+// });
