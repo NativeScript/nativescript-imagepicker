96e271751f3f440877fb59649c564b8583c93f18
Veselina Radeva <Veselina.Radeva@progress.com>
2017-08-02 10:47:41 +0300

    chore: formatting

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,28 +1,25 @@
-import * as frame from "tns-core-modules/ui/frame";
-import * as platform from "tns-core-modules/platform";
+import { EventData } from 'tns-core-modules/data/observable';
+import { Page } from 'tns-core-modules/ui/page';
+import { isAndroid } from "tns-core-modules/platform";
 import * as imagepicker from "nativescript-imagepicker";
 import * as permissions from "nativescript-permissions";
 
-let page;
 let list;
 
-function pageLoaded(args) {
-    page = args.object;
+export function pageLoaded(args: EventData) {
+    let page = <Page>args.object;
     list = page.getViewById("urls-list");
 }
-exports.pageLoaded = pageLoaded;
 
-function onSelectMultipleTap(args) {
+export function onSelectMultipleTap(args) {
     let context = imagepicker.create({ mode: "multiple" });
     startSelection(context);
 }
-exports.onSelectMultipleTap = onSelectMultipleTap;
 
-function onSelectSingleTap(args) {
+export function onSelectSingleTap(args) {
     let context = imagepicker.create({ mode: "single" });
     startSelection(context);
 }
-exports.onSelectSingleTap = onSelectSingleTap;
 
 function startSelection(context) {
     requestPermissions()
@@ -49,10 +46,9 @@ function startSelection(context) {
 }
 
 function requestPermissions() {
-    if (platform.isAndroid && (<any>android).os.Build.VERSION.SDK_INT >= 23) {
+    if (isAndroid && (<any>android).os.Build.VERSION.SDK_INT >= 23) {
         return permissions.requestPermission([(<any>android).Manifest.permission.READ_EXTERNAL_STORAGE]);
     } else {
         return Promise.resolve();
     }
 }
-exports.requestPermissions = requestPermissions;
