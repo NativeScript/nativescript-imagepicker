1af7f8142f6734d13f7471144093e93715f042fd
Teodor Bozhikov <Teodor.Bozhikov@progress.com>
2018-03-22 17:34:11 +0200

    refactor: use viewModel and bindings in demo

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,50 +1,12 @@
 import { EventData } from 'tns-core-modules/data/observable';
 import { Page } from 'tns-core-modules/ui/page';
-import { isAndroid } from "tns-core-modules/platform";
-import * as imagepicker from "nativescript-imagepicker";
+import { MainViewModel } from './main-view-model';
 
-let list;
-let imageSrc;
+let viewModel;
 
 export function pageLoaded(args: EventData) {
+    viewModel = viewModel || new MainViewModel();
+
     let page = <Page>args.object;
-    list = page.getViewById("urls-list");
-    imageSrc = page.getViewById("imageSrc");
-}
-
-export function onSelectMultipleTap(args) {
-    let context = imagepicker.create({ mode: "multiple" });
-    startSelection(context, false);
-}
-
-export function onSelectSingleTap(args) {
-    let context = imagepicker.create({ mode: "single" });
-    startSelection(context, true);
-}
-
-function startSelection(context, isSingle) {
-    context
-        .authorize()
-        .then(function() {
-            list.items = [];
-            return context.present();
-        })
-        .then(function(selection) {
-            console.log("Selection done:");
-            selection.forEach(function(selected) {
-                console.log("----------------");
-                console.log("uri: " + selected.uri);
-                if (isSingle) {
-                    selected.getImage({ maxWidth: 200, maxHeight: 200, aspectRatio: 'fill' })
-                    .then((imageSource) => {
-                        imageSrc.src = imageSource;
-                    });
-                } else {
-                    imageSrc.visibility = 'hidden';
-                }
-            });
-            list.items = selection;
-        }).catch(function (e) {
-            console.log(e);
-        });
+    page.bindingContext = viewModel;
 }
---
+++ demo/app/main-view-model.ts
@@ -0,0 +1,74 @@
+import { Observable } from 'tns-core-modules/data/observable';
+import { isAndroid } from "tns-core-modules/platform";
+import * as imagepicker from "nativescript-imagepicker";
+import { ObservableArray } from 'tns-core-modules/data/observable-array/observable-array';
+
+export class MainViewModel extends Observable {
+    constructor() {
+        super();
+    }
+
+    private _imageSrc: any;
+    private _imageAssets: Array<any>;
+
+    get imageSrc(): any {
+        return this._imageSrc;
+    }
+
+    set imageSrc(value: any) {
+        if (this._imageSrc !== value) {
+            this._imageSrc = value;
+            this.notifyPropertyChange('imageSrc', value);
+        }
+    }
+
+    get imageAssets(): any {
+        return this._imageAssets;
+    }
+
+    set imageAssets(value: any) {
+        if (this._imageAssets !== value) {
+            this._imageAssets = value;
+            this.notifyPropertyChange('imageAssets', value);
+        }
+    }
+
+    public onSelectMultipleTap(args) {
+        let context = imagepicker.create({ mode: "multiple" });
+        this.startSelection(context, false);
+    }
+
+    public onSelectSingleTap(args) {
+        let context = imagepicker.create({ mode: "single" });
+        this.startSelection(context, true);
+    }
+
+    private startSelection(context, isSingle) {
+        context
+            .authorize()
+            .then(() => {
+                this.imageAssets = [];
+                return context.present();
+            })
+            .then((selection) => {
+                console.log("Selection done:");
+                selection.forEach((selected) => {
+                    console.log("----------------");
+                    console.log("uri: " + selected.uri);
+                    if (isSingle) {
+                        // selected.getImage({ maxWidth: 200, maxHeight: 200, aspectRatio: 'fill' })
+                        // .then((imageSource) => {
+                        //     imageSrc.src = imageSource;
+                        // });
+                        selected.options = { width: 200, height: 200 };
+                        this.imageSrc = selected;
+                    } else {
+                        this.imageSrc = null;
+                    }
+                });
+                this.imageAssets = selection;
+            }).catch(function (e) {
+                console.log(e);
+            });
+    }
+}
