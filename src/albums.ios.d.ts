db4f6d582cdb15a3416598bbeb82b246b0eebda8
Teodor Bozhikov <Teodor.Bozhikov@progress.com>
2018-04-04 11:49:26 +0300

    refactor: cleanup unneeded files

--- src/albums.ios.d.ts
+++
@@ -1,6 +0,0 @@
-import { Page } from "tns-core-modules/ui/page";
-export declare function onAlbumsItemTap(args: any): void;
-export declare function pageLoaded(args: any): void;
-export declare function navigatedFrom(args: any): void;
-export declare function done(args: any): void;
-export declare function albumsPageFactory(): Page;
