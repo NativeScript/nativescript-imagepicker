aba87e29ced3b87fb2227083f7e655df042c4c0d
sis0k0 <stanimira.vlaeva@gmail.com>
2018-05-16 14:57:46 +0300

    chore: update to webpack 4 and nativescript-dev-webpack next

--- demo/app/app.ts
+++ demo/app/app.ts
@@ -1,5 +1,4 @@
-﻿import "./bundle-config";
-import * as application from 'tns-core-modules/application';
+﻿import * as application from 'tns-core-modules/application';
 
 application.setCssFileName("./app.css");
 application.start({ moduleName: "main-page" });
